b82787e14f5494d596e8573eee2ee840d3cb5005
simon23-12 <143451808+simon23-12@users.noreply.github.com>
2023-08-28 22:27:18 +0200

    Add files via upload

---
+++ screenscraper.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+const desktopPath = path.join(require('os').homedir(), 'Desktop');
+
+async function takeScreenshot() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const url = 'https://roomcheck-git-updatedproduction-ekat-br.vercel.app/teacher'; // Replace with the desired URL
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+  // Scroll to the end of the page
+  await autoScroll(page);
+
+  const timestamp = new Date().toISOString().replace(/:/g, '-');
+  const screenshotPath = path.join(desktopPath, `screenshot-${timestamp}.png`);
+
+  // Capture the full page
+  await page.screenshot({ path: screenshotPath, fullPage: true });
+
+  await browser.close();
+}
+
+async function autoScroll(page){
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      var totalHeight = 0;
+      var distance = 100;
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if(totalHeight >= scrollHeight){
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
+
+async function main() {
+  try {
+    setInterval(async () => {
+      await takeScreenshot();
+      console.log('Screenshot captured and saved.');
+    }, 30000);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+main();
